c1e32a96b502b1cf5a2631eb3d30c185fb160829
MDybek <dybek8780@gmail.com>
2023-05-22 21:44:55 +0200

    first steps on main page

--- frontend/mainSiteFunctionality.js
+++ frontend/mainSiteFunctionality.js
@@ -6,11 +6,36 @@ document.addEventListener("DOMContentLoaded", function () {
             data.forEach(post => {
                 const tile = document.createElement("div");
                 tile.className = "tile";
-                tile.textContent = post.title;
+
+                const title = document.createElement("h2");
+                title.className = "title";
+                title.textContent = post.title;
+                tile.appendChild(title);
+
+                const createdAt = document.createElement("p");
+                createdAt.className = "created-at";
+
+                const date = new Date(post.created_at);
+                const options = {year: 'numeric', month: 'long', day: 'numeric'};
+                const formattedDate = date.toLocaleDateString(undefined, options);
+
+                createdAt.textContent = "Created at: " + formattedDate;
+                tile.appendChild(createdAt);
+
+
+                // const tags = document.createElement("p");
+                // tags.textContent = "Tags: "+ post.tags;
+                // tile.appendChild(tags);
+
+                const image = document.createElement("img");
+                image.src = post.image;
+                image.alt = "Post Image";
+                tile.appendChild(image);
+
                 tileContainer.appendChild(tile);
             });
         })
         .catch(error => {
-            console.error("Błąd pobierania danych:", error);
+            console.error("Error while loading data:", error);
         });
 });
